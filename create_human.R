f27446ccbce5b16a76bce4a29a8c03cc966645bc
Justin Box <44566821+justinrbox@users.noreply.github.com>
2018-12-02 20:05:31 +0200

    Add files via upload

--- create_human.R
+++ create_human.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(data.table)
 library(stringr)
+library(tidyr)
+# Link to the metadata (http://hdr.undp.org/en/content/human-development-index-hdi)
 
 ##Reading the Data
 
@@ -33,22 +35,22 @@ colnames(gii)
 
 ##Rename
 hd_old = c("HDI.Rank", "Country", "Human.Development.Index..HDI.", "Life.Expectancy.at.Birth", "Expected.Years.of.Education", "Mean.Years.of.Education", "Gross.National.Income..GNI..per.Capita", "GNI.per.Capita.Rank.Minus.HDI.Rank")
-hd_new = c("HDI.Rank", "Country", "HDI", "L.Exp.Birth", "Exp.Yr.Ed", "Mean.Yr.Ed", "GNIperCap", "GNIperCap-HDI.Rank")
+hd_new = c("HDI.Rank", "Country", "HDI", "Life.Exp", "Edu.Exp", "Mean.Yr.Ed", "GNIperCap", "GNIperCap-HDI.Rank")
 
 setnames(hd, old=hd_old, new=hd_new)
 
 colnames(hd)
 
 gii_old = c("GII.Rank", "Country", "Gender.Inequality.Index..GII.", "Maternal.Mortality.Ratio", "Adolescent.Birth.Rate", "Percent.Representation.in.Parliament", "Population.with.Secondary.Education..Female.", "Population.with.Secondary.Education..Male.", "Labour.Force.Participation.Rate..Female.", "Labour.Force.Participation.Rate..Male.")
-gii_new = c("GII.Rank", "Country", "GII", "Mat.Mort.Rat", "Ad.Birth.Rate", "%Rep.in.Par", "Pop.w.2nd.Ed.F", "Pop.w.2nd.Ed.M", "Lab.Part.Rate.F", "Lab.Part.Rate.M")
+gii_new = c("GII.Rank", "Country", "GII", "Mat.Mor", "Ado.Birth", "Parli.F", "Edu2.F", "Edu2.M", "Labo.F", "Labo.M")
 
 setnames(gii, old=gii_old, new=gii_new)
 colnames(gii)
 
 ##Mutate
 
-gii <- mutate(gii, Edu2.FM = Pop.w.2nd.Ed.F/Pop.w.2nd.Ed.M)
-gii <- mutate(gii, Labo.FM = Lab.Part.Rate.F/Lab.Part.Rate.M)
+gii <- mutate(gii, Edu2.FM = Edu2.F/Edu2.M)
+gii <- mutate(gii, Labo.FM = Labo.F/Labo.M)
 
 ##Joining the Datasets
 
@@ -62,3 +64,61 @@ write.csv(human, file=fid, row.names = FALSE)
 
 ##Re-Read
 read_data <- read.table(fid, header=TRUE, sep=",")
+
+colnames(human)
+
+##Description of the Data
+
+"HDI.Rank" = Country rank in Human Development Index
+"Country"            
+"HDI" = Human Development Index              
+"L.Exp.Birth" = Life expectancy at birth      
+"Exp.Yr.Ed" = Expected years of schooling      
+"Mean.Yr.Ed" = Mean years of schooling       
+"GNIperCap" = Gross national income (GNI) per capita        
+"GNIperCap-HDI.Rank" = Gross national income (GNI) per capita - Human Development Index 
+"GII.Rank"= Country rank in Gender Inequality Index
+"GII" = Gender Inequality Index              
+"Mat.Mort.Rat" = Maternal mortality ratio      
+"Ad.Birth.Rate" = Adolescent birth rate    
+"%Rep.in.Par"  = Percetange of female representatives in parliament       
+"Pop.w.2nd.Ed.F"  = Population of women with at least some secondary education
+"Pop.w.2nd.Ed.M"  = Population of men with at least some secondary education
+"Lab.Part.Rate.F" = Population of women participating in labor
+"Lab.Part.Rate.M" = Population of men participating in labor
+"Edu2.FM"  = The ratio of Female / Male populations with secondary education (edu2F / edu2M)
+"Labo.FM"  = The ratio of Female / Male participation in labour force (work2F / work2M)
+----------------------------
+# transform the Gross National Income (GNI) variable to numeric
+library(stringr)
+
+# remove the commas from GNI and print out a numeric version of it
+gni_numeric <- str_replace(human$gni, pattern=",", replace ="") %>% as.numeric
+
+# mutate
+human$GNI <- str_replace(human$GNI, ",", "")
+human$GNI <- as.numeric(human$GNI)
+
+# keep only these columns
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- dplyr::select(human, one_of(keep))
+
+# remove rows with missing values 
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human <- filter(human, complete.cases(human))
+
+# remove regions that comprise the last 7 rows
+tail(human, n=10L)
+last <- nrow(human) - 7
+human_ <- human[1:last, ]
+
+
+rownames(human_) <- human_$Country # add countries as rownames
+human_ <- dplyr::select(human_, -Country) # remove the Country variable
+dim(human_) # 155 observations and 8 variables
+
+write.csv(human_, file="human.csv", row.names = TRUE) # write data to file
+# test <- read.csv(file="human.csv")
+
+
